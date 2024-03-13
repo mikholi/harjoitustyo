74f08a828f228141e87d77c26ca5b57dc858f0cd
Mikko Keränen <148424192+mikholi@users.noreply.github.com>
2024-03-13 07:07:13 +0200

    Update paasivu.js

--- jscript/paasivu.js
+++ jscript/paasivu.js
@@ -1,4 +1,4 @@
-// Tämä funktio odottaa että kuluu kymmenen sekuntia ja siirtyy pääsivulle
+
 setTimeout(function() {
     window.location.href = 'index.html' // Tässä osoite sivulle mihin siirrytään
 }, 5000) // 
